0ae385e0d90c345509d9d4a22fb22f5a2d6861c2
Nayan Chakrabarty <nayandcc5@gmail.com>
2020-03-10 10:03:09 +0600

    commit

---
+++ app/src/main/java/com/nexttech/sathethakun/NotificationActitivity.java
@@ -0,0 +1,14 @@
+package com.nexttech.sathethakun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificationActitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_actitivity);
+    }
+}
