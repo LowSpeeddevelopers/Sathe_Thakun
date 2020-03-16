617f977349e674e2fd42f73eddc7d4aae1679559
HridoyHasan <hridoyhasan14@gmail.com>
2020-03-16 13:12:12 +0600

    registration field fixed

---
+++ app/src/main/java/com/nexttech/sathethakun/LoginFragment.java
@@ -0,0 +1,97 @@
+package com.nexttech.sathethakun;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginFragment extends Fragment {
+    Context context;
+    EditText edtLoginEmail, edtLoginPassword;
+    Button btnLogin;
+    TextView tvSignup;
+
+    private FirebaseAuth mAuth;
+
+    String email, password;
+
+    public LoginFragment(Context context){
+        this.context = context;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        mAuth = FirebaseAuth.getInstance();
+        View vi = inflater.inflate(R.layout.fragment_login,container,false);
+
+
+
+
+        edtLoginEmail = vi.findViewById(R.id.edt_login_email);
+        edtLoginPassword = vi.findViewById(R.id.edt_login_password);
+        btnLogin = vi.findViewById(R.id.btn_login);
+        tvSignup = vi.findViewById(R.id.tv_signup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = edtLoginEmail.getText().toString();
+                password = edtLoginPassword.getText().toString();
+
+                if (email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(context, "Input Data", Toast.LENGTH_SHORT).show();
+                } else {
+                    loginUser();
+                }
+            }
+        });
+
+        tvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginandRegisterholder.updateFragment(getFragmentManager().beginTransaction(),new RegisterFragment(context),context);
+            }
+        });
+
+
+        return vi;
+    }
+
+
+    private void loginUser() {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(((Activity)context), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            MainActivity.updateUI(user,context);
+                        } else {
+                            Toast.makeText(context, "Authentication failed.",Toast.LENGTH_SHORT).show();
+                            MainActivity.updateUI(null,context);
+                        }
+                    }
+                });
+    }
+
+}
---
+++ app/src/main/java/com/nexttech/sathethakun/RegisterFragment.java
@@ -0,0 +1,134 @@
+package com.nexttech.sathethakun;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.nexttech.sathethakun.Model.UserModel;
+
+public class RegisterFragment extends Fragment {
+    Context context;
+    public RegisterFragment(Context context){
+        this.context = context;
+    }
+
+    EditText edtSignupName, edtSignupEmail, edtSignupPhone, edtSignupAge, edtSignupAddress, edtSignupPassword, edtSignupRetypePassword;
+    Button btnSignup;
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+
+    String name, email, phone, age, address, password, retypePassword;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        View vi;
+
+
+        vi = inflater.inflate(R.layout.fragment_register,container,false);
+
+
+
+
+        edtSignupName = vi.findViewById(R.id.edt_signup_name);
+        edtSignupEmail = vi.findViewById(R.id.edt_signup_email);
+        edtSignupPassword = vi.findViewById(R.id.edt_signup_password);
+        edtSignupRetypePassword = vi.findViewById(R.id.edt_signup_retype_password);
+        edtSignupPhone = vi.findViewById(R.id.edt_signup_phone);
+        edtSignupAge = vi.findViewById(R.id.edt_signup_age);
+        edtSignupAddress = vi.findViewById(R.id.edt_signup_address);
+
+        btnSignup = vi.findViewById(R.id.btn_signup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = edtSignupName.getText().toString();
+                email = edtSignupEmail.getText().toString();
+                phone = edtSignupPhone.getText().toString();
+                age = edtSignupAge.getText().toString();
+                address = edtSignupAddress.getText().toString();
+                password = edtSignupPassword.getText().toString();
+                retypePassword = edtSignupRetypePassword.getText().toString();
+
+                if (name.isEmpty() || email.isEmpty() || phone.isEmpty() || age.isEmpty() || address.isEmpty() || password.isEmpty() || retypePassword.isEmpty()){
+                    Toast.makeText(context, "Input Data", Toast.LENGTH_SHORT).show();
+                } else if (!password.equals(retypePassword)){
+                    Toast.makeText(context, "Password & Retype Password not same.", Toast.LENGTH_SHORT).show();
+                } else {
+                    createUser();
+                }
+            }
+        });
+
+
+        return vi;
+
+    }
+
+    private void createUser() {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(((Activity)context), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            insertIntoDatabase(user.getUid());
+                            MainActivity.updateUI(user,context);
+
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(context, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            Log.e("reasone",task.getException().toString());
+                            MainActivity.updateUI(null,context);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void insertIntoDatabase(String uid) {
+
+        DatabaseReference myRef = database.getReference("Users").child(uid);
+
+        UserModel userModel = new UserModel(name, email, phone, age, address);
+
+        myRef.setValue(userModel).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(context,"successful",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(context,"unsuccessful",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+}
