5af22008366801c6d287eaf3f59c6fe905574e55
Shahariar Newaz Taki <43105661+shahariartaki@users.noreply.github.com>
2020-04-01 22:21:56 +0600

    help_done

--- app/src/main/java/com/nexttech/sathethakun/Fragments/HelpFragment.java
+++ app/src/main/java/com/nexttech/sathethakun/Fragments/HelpFragment.java
@@ -73,32 +73,42 @@ public class HelpFragment extends Fragment {
         listDataChild = new HashMap<>();
 
         // Adding group data
-        listDataGroup.add("Help 1");
-        listDataGroup.add("Help 2");
-        listDataGroup.add("Help 3");
+        listDataGroup.add("How can I Registration New Account");
+        listDataGroup.add("How can I Sing in");
+        listDataGroup.add("How can I Search People");
+        listDataGroup.add("How can I Tracking People");
 
         // Adding child data List one
         List<String> help1 = new ArrayList<>();
-        help1.add("1");
-        help1.add("2");
-        help1.add("3");
+        help1.add("Click on Sign Up Button ");
+        help1.add("Put Your Personal  information");
+        help1.add("then Click on Sign Up Button, which is available on bottom");
 
         // Adding child data List two
         List<String> help2  = new ArrayList<>();
-        help2.add("1");
-        help2.add("2");
-        help2.add("3");
+        help2.add("put your email address ");
+        help2.add("put your password");
+        help2.add("then Click on Sign In Button");
 
         // Adding child data List three
         List<String> help3 = new ArrayList<>();
-        help3.add("1");
-        help3.add("2");
-        help3.add("3");
-        help3.add("4");
+        help3.add("you would find those people which are connected with this application");
+        help3.add("Click Add People");
+        help3.add("put people phone number");
+        help3.add("then click on search button ");
+        help3.add("if you find right person you can send request them");
+        help3.add("if he/she accept your request, you can see them on connected people");
+
+        List<String> help4 = new ArrayList<>();
+        help4.add("click on connected people ");
+        help4.add("Click on Location Button ");
+        help4.add("then maps will open and see realtime location which is connected with you");
+
 
         listDataChild.put(listDataGroup.get(0), help1); // Header, Child data
         listDataChild.put(listDataGroup.get(1), help2); // Header, Child data
         listDataChild.put(listDataGroup.get(2), help3); // Header, Child data
+        listDataChild.put(listDataGroup.get(3), help4); // Header, Child data
 
         HelpExpandableListAdapter listAdapter = new HelpExpandableListAdapter(context, listDataGroup, listDataChild);
         list_view.setAdapter(listAdapter);
